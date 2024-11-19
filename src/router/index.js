b1e51f7822d1a19270399248f8b4392c3188b0bc
Daniel Truque <truquemdaniels.cla@gmail.com>
2024-11-18 22:50:44 -0500

    Personas Vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import HomeView from '../views/HomeView.vue';
+import Comunas from '../views/Comunas.vue';
 
 const routes = [
   {
@@ -8,13 +10,18 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/comunas',
+    name: 'Comunas',
+    component: Comunas
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
 ]
 
 const router = createRouter({
